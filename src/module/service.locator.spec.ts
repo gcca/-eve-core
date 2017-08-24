63a8c5714c36152693963783c6c537bcf4d301f4
cristhian.gz <gcca@gcca.tk>
2017-08-23 23:20:58 -0500

    Add book filename locator

--- src/module/service.locator.spec.ts
+++ src/module/service.locator.spec.ts
@@ -8,11 +8,15 @@ describe('ServiceLocator', () => {
     }
 
     interface BookFinder {
-      find(filename: string): Book[];
+      find(): Book[];
     }
 
     interface BookReader {
-      read(filename: string): string;
+      read(): string;
+    }
+
+    interface BookFilenameLocator {
+      bookFilename: string;
     }
 
     interface BookFinderLocator {
@@ -25,30 +29,34 @@ describe('ServiceLocator', () => {
 
     class BookServiceLocator
         extends ServiceLocator
-        implements BookFinderLocator,
+        implements BookFilenameLocator,
+                   BookFinderLocator,
                    BookReaderLocator {
 
-      constructor(public bookFinder: BookFinder,
+      constructor(public bookFilename: string,
+                  public bookFinder: BookFinder,
                   public bookReader: BookReader) { super(); }
     }
 
     class HorrorBookFinder implements BookFinder {
-      find(filename: string) {
+      find() {
         const locator = BookServiceLocator.locator<BookServiceLocator>();
         const reader = locator.bookReader
-        const books = reader.read(filename);
+        const books = reader.read();
         return books.split('\n').map(line =>
           (([author, title]) => ({ author, title }))(line.split(':')));
       }
     }
 
     class ColonBookReader implements BookReader {
-      read(filename: string): string {
-        if ('books' == filename) {
+      read(): string {
+        const locator = BookServiceLocator.locator<BookServiceLocator>();
+        const filename = locator.bookFilename;
+        if ('colon_horror_books' == filename) {
           return ('H.P. Lovecraft:The Dunwich Horror\n' +
                   'Stephen King: DOCTOR SLEEP');
         }
-        throw new Error(`File ${filename} not found`);
+        throw new Error(`File '${filename}' not found`);
       }
     }
 
@@ -56,16 +64,18 @@ describe('ServiceLocator', () => {
       booksWrittenBy(author: string): Book[] {
         const locator = BookServiceLocator.locator<BookServiceLocator>();
         const finder = locator.bookFinder;
-        const books = finder.find('books');
+        const books = finder.find();
         return books.filter(book => author == book.author);
       }
     }
 
     it('should locate finder and reader to list', () => {
-      BookServiceLocator.load(new BookServiceLocator(new HorrorBookFinder,
+      BookServiceLocator.load(new BookServiceLocator('colon_horror_books',
+                                                     new HorrorBookFinder,
                                                      new ColonBookReader));
       const bookLister = new BookLister();
       const books = bookLister.booksWrittenBy('H.P. Lovecraft');
+      expect(books).not.to.be.null;
       expect('The Dunwich Horror').to.equal(books[0].title);
     });
   });
