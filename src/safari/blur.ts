9c315bdd1e54f74a7ce1678f80cafc7cdd2b5c29
gcca <gcca@gcca.tk>
2017-08-26 10:31:29 -0500

    Descript safari blur workaround

--- src/safari/blur.ts
+++ src/safari/blur.ts
@@ -1,3 +1,13 @@
+/**
+ * Workaround to blur on iOS empty layers.
+ *
+ * Attaching listener on body does not cause click event on iOS Safari
+ * when target has no click event listener.
+ *
+ * @see [Safari mouse emulation](https://developer.apple.com/library/content
+ *   /documentation/AppleApplications/Reference/SafariWebContent/HandlingEvents
+ *   /HandlingEvents.html#//apple_ref/doc/uid/TP40006511-SW4)
+ */
 export function blur(callback: Function): void {
   function bodyClick(event: Event) {
     callback.apply(event.target, arguments);
